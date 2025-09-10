17983608d8f9d00cfc82b3372af4712de8ccbeb9
melgamous <mimouneelgamoussi2@gmail.com>
2025-09-10 22:00:19 +0100

    first commit

---
+++ backend/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+
+	fs := http.FileServer(http.Dir("frontend"))
+
+http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "frontend/index.html")
+})
+
+log.Println("Server started on http://localhost:8080")
+
+err := http.ListenAndServe(":8080", nil)
+if err != nil {
+    log.Fatal(err)
+}
+
+}
